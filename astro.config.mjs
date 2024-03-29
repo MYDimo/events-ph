81d4c6e789356bc6cc910df1d6e0cec5ff821cc9
Mihaylo Dimo <mihaylo.dimo@gmail.com>
2024-03-29 17:39:51 +0200

    Add desktop navigation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import { astroImageTools } from "astro-imagetools";
-
 import tailwind from "@astrojs/tailwind";
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroImageTools]
+  prefetch: false,
+  integrations: [tailwind(), astroImageTools, alpinejs({ entrypoint: '/src/entrypoint' })]
 });
