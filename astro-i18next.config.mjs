97db66994063441e2515f5664c2f6fb7d3a36a4a
Mihaylo Dimo <mihaylo.dimo@gmail.com>
2024-04-22 00:30:53 +0300

    Add i18next for astro localization

---
+++ astro-i18next.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('astro-i18next').AstroI18nextConfig} */
+export default {
+  defaultLocale: "bg",
+  locales: ["bg", "en"],
+  routes: {
+    en: {
+      "за-мен": "about",
+      моменти: "moments",
+      кръщенета: "baby-showers",
+      концерти: "concerts",
+      моменти: "moments",
+      портрети: "portraits",
+      цени: "prices",
+      сватби: "weddings",
+    },
+  },
+};
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
+import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,6 +9,7 @@ export default defineConfig({
 	integrations: [
 		tailwind(),
 		alpinejs({ entrypoint: "/src/entrypoint" }),
+		astroI18next()
 	],
 	i18n: {
 		defaultLocale: "bg",
