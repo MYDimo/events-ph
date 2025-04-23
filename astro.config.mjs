7e7263b0877881c1a02fb5ceaf773033c0bb3f40
Mihaylo Dimo <mihaylo.dimo@gmail.com>
2025-04-23 23:54:07 +0300

    sync config file with what is actually available as npm package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import alpinejs from "@astrojs/alpinejs";
 import astroI18next from "astro-i18next";
 
 import mdx from "@astrojs/mdx";
-
-import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
@@ -13,10 +10,8 @@ export default defineConfig({
     prefetch: false,
     integrations: [
       tailwind(),
-      alpinejs({ entrypoint: "/src/entrypoint" }),
       astroI18next(),
       mdx(),
-      react(),
       svelte()
     ]
 });
